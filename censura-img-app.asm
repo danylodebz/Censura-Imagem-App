c809ff309a4edc9cf67f905c0576d8b90c57f51c
Danylo <nunesdanylo16@gmail.com>
2023-11-05 05:36:29 -0300

    feat: Refatoração do código Assembly para leitura de imagem BMP

--- censura-img-app.asm
+++ censura-img-app.asm
@@ -1,64 +1,77 @@
-.386
+.686
 .model flat, stdcall
 option casemap:none
 
 include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
-include \masm32\include\user32.inc
-include \masm32\include\gdi32.inc
+include \masm32\include\masm32.inc
 includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
 includelib \masm32\lib\user32.lib
-includelib \masm32\lib\gdi32.lib
 
 .data
-    fileName db "exemplo.bmp",0
-    imgWidth dd 0
-    imgHeight dd 0
+file_name db "input.bmp", 0
+output_file_name db "output.bmp", 0
 
-    WINDOW_NAME db "BMP Viewer",0 ; Defina o nome da janela
+fileHandle HANDLE 0
+outputFileHandle HANDLE 0
+byteCount dd 0
+headerBuffer db 54 dup(0)
+imageBuffer db 6480 dup(0)
 
 .code
 start:
-    invoke LoadImage, 0, addr fileName, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE
-    test eax, eax
-    jz imageError
+    ; Abrir o arquivo de entrada
+    invoke CreateFile, addr file_name, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
+    mov fileHandle, eax
+    cmp fileHandle, INVALID_HANDLE_VALUE
+    je FileOpenError
 
-    invoke GetObject, eax, sizeof BITMAP, addr imgWidth
-    mov eax, imgWidth.bmWidth
-    mov imgWidth, eax
-    mov eax, imgWidth.bmHeight
-    mov imgHeight, eax
+    ; Abrir o arquivo de saída
+    invoke CreateFile, addr output_file_name, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    mov outputFileHandle, eax
+    cmp outputFileHandle, INVALID_HANDLE_VALUE
+    je FileCreationError
 
-    invoke CreateWindowEx, 0, addr CLASS_NAME, addr WINDOW_NAME, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, imgWidth, imgHeight, NULL, NULL, NULL, NULL
-    test eax, eax
-    jz windowError
+    ; Ler o cabeçalho da imagem
+    invoke ReadFile, fileHandle, addr headerBuffer, 54, addr byteCount, 0
+    invoke WriteFile, outputFileHandle, addr headerBuffer, 54, addr byteCount, 0
 
-    invoke ShowWindow, eax, SW_SHOWNORMAL
-    invoke UpdateWindow, eax
+    ; Ler o restante da imagem e gravar no arquivo de saída
+    invoke SetFilePointer, fileHandle, 54, NULL, FILE_BEGIN
 
-    .while TRUE
-        invoke GetMessage, addr msg, NULL, 0, 0
-        .break .if (!eax)
-        invoke TranslateMessage, addr msg
-        invoke DispatchMessage, addr msg
-    .endw
+    readLoop:
+        invoke ReadFile, fileHandle, addr imageBuffer, 6480, addr byteCount, 0
+        test eax, eax
+        jz readLoop_concluid
+        invoke WriteFile, outputFileHandle, addr imageBuffer, byteCount, addr byteCount, 0
+        jmp readLoop
 
+    readLoop_concluid:
+
+    ; Fechar o arquivo de entrada
+    invoke CloseHandle, fileHandle
+
+    ; Fechar o arquivo de saída
+    invoke CloseHandle, outputFileHandle
+
+    ; Encerrar o programa
     invoke ExitProcess, 0
 
-imageError:
-    invoke MessageBox, NULL, addr fileName, addr fileName, MB_OK
+FileOpenError:
+    ; Lidar com erro na abertura do arquivo de entrada
+    jmp Exit
+
+FileCreationError:
+    ; Lidar com erro na criação do arquivo de saída
+    jmp Exit
+
+Exit:
+    ; Encerrar o programa
     invoke ExitProcess, 1
-
-windowError:
-    invoke MessageBox, NULL, addr WINDOW_NAME, addr WINDOW_NAME, MB_OK
-    invoke ExitProcess, 1
-
-.const
-    CLASS_NAME db "BMPViewer", 0
-
-.data?
-    msg MSG
-    hwnd dd ?
 
 end start
+
+
+
 
